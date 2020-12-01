67d1e6a589fade205dc4943a2fd0e7057e608779
lord <lord@localhost.localdomain>
2020-12-01 17:16:20 +0100

    commit day 1

---
+++ day1.py
@@ -0,0 +1,47 @@
+def readFile():
+    number_list = []
+
+    file = open("input1.1", "r")
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        number_list.append(int(line))
+    file.close()
+
+    return number_list
+
+
+def two_sum_to(number, num_list, starting_index):
+    for i in range(starting_index, len(num_list)):
+        for j in range(i+1, len(num_list)):
+            if num_list[i]+num_list[j] == number:
+                return [num_list[i], num_list[j]]
+    return []
+
+
+def three_sum_to(number, num_list, starting_index):
+    for i in range(starting_index, len(num_list)):
+        temp = two_sum_to(number-num_list[i], num_list, i+1)
+        if len(temp) != 0:
+                temp.append(num_list[i])
+                return temp
+    return []
+
+
+def main():
+    num_list = readFile()
+
+    result = two_sum_to(2020, num_list, 0)
+    print(result[:])
+    print(str(result[0] * result[1]))
+    print()
+
+    result = three_sum_to(2020, num_list, 0)
+    print(result[:])
+    print(str(result[0] * result[1] * result[2]))
+    print()
+
+
+if __name__ == '__main__':
+    main()
